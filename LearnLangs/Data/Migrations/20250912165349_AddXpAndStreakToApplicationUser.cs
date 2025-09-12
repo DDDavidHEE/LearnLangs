8a10e11abdb542533f27d17d3ec50fa464b9112a
DavidHE <davidhoangem@gmail.com>
2025-09-13 00:14:16 +0700

    Fix: Display XP and Streak in navbar

---
+++ LearnLangs/Data/Migrations/20250912165349_AddXpAndStreakToApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnLangs.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddXpAndStreakToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
