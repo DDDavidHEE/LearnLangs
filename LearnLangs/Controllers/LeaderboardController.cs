72044ddca2ba067a51a86bb0bc069354fa19b4e7
DavidHE <davidhoangem@gmail.com>
2025-09-13 00:58:19 +0700

    Implement Leaderboard page and controller

---
+++ LearnLangs/Controllers/LeaderboardController.cs
@@ -0,0 +1,32 @@
+﻿using LearnLangs.Data;
+using LearnLangs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace LearnLangs.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LeaderboardController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Leaderboard
+        public async Task<IActionResult> Index()
+        {
+            // Get the top users ordered by XP
+            var topUsers = await _context.Users
+                                         .OrderByDescending(u => u.TotalXP)  // Order by XP
+                                         .Take(10)  // Limit to top 10 users
+                                         .ToListAsync();
+
+            return View(topUsers);
+        }
+    }
+}
